444bfde274556d68bc61f0f9b958bdbbac508b0f
Josh Gamache <jsgamache@gmail.com>
2019-08-16 20:10:20 +0000

    more changes

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="RoboFriends" src="" />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@robofriends.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
