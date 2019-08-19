bad541f0b6a9074801adc08290b79431cdf01ca3
Josh Gamache <jsgamache@gmail.com>
2019-08-19 14:38:24 +0000

    Add multiple cards and data. Variables in card

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => { // {} destructures "props"
     return (
-        <div>
-            <img alt="RoboFriends" src="" />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt="RoboFriends" src={`https://robohash.org/${id}?size=200x200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@robofriends.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
