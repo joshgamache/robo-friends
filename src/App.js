f48d50fdd25f8dd9575ef10608a60cf9aa38b96b
Infiniwild <jsgamache@gmail.com>
2019-08-25 14:53:52 -0600

    filtering on search todo more

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 
-const App = () => {
-    return (
-        <div>
-            <h1 className="f1 h1-title tc">RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+const state = {
+}
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    
+    onSearchChange(event){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="tc">
+                <h1 >RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
